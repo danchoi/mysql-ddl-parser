d4138ea3537487a969c77f61d2de9587f8700c60
Daniel Choi <dhchoi@gmail.com>
2012-10-19 09:28:03 -0400

    trying some workaround for unknown longblob

--- convert.hs
+++ convert.hs
@@ -5,7 +5,8 @@ import Control.Monad
 import Database.HDBC
 import Database.HDBC.MySQL
 import Database.HDBC.PostgreSQL
-import Data.List (intercalate)
+import Data.List (intercalate, elemIndex, findIndices)
+import qualified Data.ByteString as B
 
 -- OS X /private/tmp/mysql.sock
 connMysql = connectMySQL defaultMySQLConnectInfo { mysqlHost = "localhost", mysqlUser = "root", mysqlDatabase = "mackey_production", mysqlUnixSocket = "/private/tmp/mysql.sock" }
@@ -16,23 +17,49 @@ connMysql = connectMySQL defaultMySQLConnectInfo { mysqlHost = "localhost", mysq
 -- target
 connPg = connectPostgreSQL "dbname=mackey"
    
+isSqlUnknown :: SqlTypeId -> Bool
+isSqlUnknown (SqlUnknownT _) = True
+isSqlUnknown _ = False
+
+getByteString :: SqlValue -> B.ByteString
+getByteString (SqlByteString x) = x
+getByteString _ = undefined
+
+
 main = do
   pg <- connPg
   mysql <- connMysql
   tables <- getTables mysql
   let tables' = filter (\t -> not $ elem t ["archived_file_uploads", "file_uploads"] ) tables
-  forM_ tables' (\t -> do 
+  forM_ tables (\t -> do 
       putStrLn t
       cols' <- map fst  `liftM` describeTable mysql t
+      colmeta <- map snd  `liftM` describeTable mysql t
+      let colmeta2 = show (map colType colmeta)
+      -- putStrLn $ show $ elemIndex (SqlUnknownT "longblob") colmeta2 
+
       let cols = map (\x -> "\"" ++ x ++ "\"") cols' -- quote all table columns 
       putStrLn $ show cols'
+
+      -- find any SqlUnknownT columns
+      let toFix = findIndices (isSqlUnknown . colType) colmeta 
+      putStrLn $ "Unknown types at " ++ (show toFix)
+
       rows <- withRTSSignalsBlocked $ do
         quickQuery' mysql ("select * from " ++ t) []
 
       forM_ rows $ \row -> do 
+        -- putStrLn $ show row  -- DEBUGGER
+
         let placeholders = intercalate "," $ take (length cols) $ repeat "?"
             query = "insert into " ++ t ++ "(" ++ (intercalate ", " cols) ++ ") values (" ++ placeholders ++ ")"
-        run pg query row
+            row' = zipWith fixUnknown row colmeta
+            fixUnknown :: SqlValue -> SqlColDesc -> SqlValue
+            fixUnknown x meta = case (isSqlUnknown . colType) meta of
+                                  True -> (SqlByteString (getByteString x))
+                                  False -> x
+                
+        run pg query row'
         putChar '.'
         commit pg
     )
